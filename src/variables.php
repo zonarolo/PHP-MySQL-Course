ab192e07ae3c3219d1f7b2bfe1dd16e1990a06d6
Juan Rolo <juanluis211@outlook.com>
2021-04-30 01:59:48 +0200

    Variables in PHP

---
+++ src/variables.php
@@ -0,0 +1,30 @@
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+
+<body>
+  <?php
+  // Variables
+  // Is a symbol or name that stands for a value
+  $name = "Juan"; // Valid
+  $Name = "Juan"; // Valid
+  // $9name = "Juan"; // Invalid; Starts with a number
+  $_name = "Juan"; // Valid
+  $_9name = "Juan"; // Valid
+
+
+  echo "My name is $name <br>";
+  echo "$name is 26 years old";
+  /**
+   * Variable names
+   * Variable name starts with a letter A-Z a-z, underscore (_), followed by any number of letters, numbers, or underscores.
+   */
+  ?>
+</body>
+
+</html>
