de5e15e9fe7b9efc8ab2e3b11c7f8d1254ad747e
Juan Rolo <juanluis211@outlook.com>
2021-04-30 02:07:35 +0200

    Multiple ways to add variables in echo/print

---
+++ src/ways.php
@@ -0,0 +1,26 @@
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+
+<body>
+  <?php
+  // Add variables in echo/print  
+  $age = 24;
+
+  // Echo
+  // Simple qoutes - Varible named are echoed as it is
+  // Double quotes - Variable names are replaced by the varible values
+
+  echo "My age is $age <br>";
+  echo 'My age is ' . $age . '<br>';
+  print "My age is $age <br>";
+  print 'My age is ' . $age . '<br>'
+  ?>
+</body>
+
+</html>
