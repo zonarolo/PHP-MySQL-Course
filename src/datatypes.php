ab9f956ec196f487300ec8fad9982e3f5aa2310a
Juan Rolo <juanluis211@outlook.com>
2021-04-30 02:16:21 +0200

    Data types

---
+++ src/datatypes.php
@@ -0,0 +1,41 @@
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+
+<body>
+  <?php
+  /**
+   * Integer
+   * Floating point or Float
+   * String
+   * Booleans
+   * Array
+   * Object
+   * Resource
+   * Null
+   */
+
+  $age = 26;
+  echo "My age is $age <br>";
+
+  $price = 10.5;
+  echo "The price is $price <br>";
+
+  $name = "Juan";
+  echo "My name is $name <br>";
+
+  $is_allowed = true;
+  echo $is_allowed . '<br>';
+
+  $x = null;
+  echo $x;
+  $y;
+  ?>
+</body>
+
+</html>
