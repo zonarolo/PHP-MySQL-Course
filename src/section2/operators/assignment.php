d0bf0bf5b7da9a3eb4eac0ca917f332e714e9dcb
Juan Rolo <juanluis211@outlook.com>
2021-05-08 14:45:26 +0200

    Assignment operators

---
+++ src/section2/operators/assignment.php
@@ -0,0 +1,34 @@
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+
+<body>
+  <?php
+  /*
+      Assignment operators
+      += ----- Add and assign
+      -= ----- Subtract and assign
+      /*= ----- Multiply and assign
+      /= ----- Divide and assign
+      .= ----- Concatenate and assign
+    */
+
+  $x = 5;
+  $y = 5;
+
+  echo $x += 5;
+  echo "<br>";
+
+  $str = "Juan is a old man";
+  $str .= " because he is 26 years old! <br>";
+
+  echo $str;
+  ?>
+</body>
+
+</html>
