67c971750664a4221997b19ef032ba9f9cdb2b20
Juan Rolo <juanluis211@outlook.com>
2021-05-17 11:13:07 +0200

    For loop

--- src/section3/while.php
+++ src/section3/while.php
@@ -30,6 +30,30 @@
     $cookie++;
   } while ($cookie < 1);
 
+
+  // Inicialization, Evaluated once at the beginning
+  // Condition, Option 1 - If its true, excute
+  // Option 2 - If its false, stop
+  // Counter, Evaluated at the end of every loop
+
+  for ($i = 1; $i <= 10; $i++) {
+    echo "The number is: " . $i . "<br>";
+  }
+
+  /**
+   * Going to the bank on the 1st of January
+   * Want to deposit 100 dollars
+   * Interest rate is 5%
+   * Withdrawl it after 5 years
+   */
+
+  $deposit = 1000;
+  $interest = 0.05;
+
+  for ($years = 1; $years <= 5; $years++) {
+    $deposit += ($deposit * $interest);
+    echo "The total amount after " . $years . " year is: " . $deposit . "<br>";
+  }
   ?>
 </body>
 
