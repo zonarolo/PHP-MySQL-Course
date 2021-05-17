4fdbd3509e6caeb52b5df8db5e4505b25fa4a46d
Juan Rolo <juanluis211@outlook.com>
2021-05-17 10:23:42 +0200

    Switch statement

---
+++ src/section3/switch-statement.php
@@ -0,0 +1,35 @@
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+
+<body>
+  <?php
+  // Conditional statement
+
+  $role = "Employee";
+
+  switch ($role) {
+    case 'Visitor':
+      echo "Welcome visitor!";
+      break;
+    case 'Admin':
+      echo "Welcome admin!";
+      break;
+    case 'Superadmin':
+      echo "Welcome Superadmin!";
+      break;
+    case 'Boss':
+      echo "Welcome Boss!";
+      break;
+    default:
+      echo "User has no role.";
+  }
+  ?>
+</body>
+
+</html>
